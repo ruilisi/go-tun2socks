6d4d5fce777c9fa8b527e5a3c712fa5c59e3f74d
Syrone Wong <wong.syrone@gmail.com>
2020-02-07 11:03:44 +0800

    make sure to include lwipopts.h for all c source files
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- core/c_custom.go
+++ core/c_custom.go
@@ -1,7 +1,7 @@
 package core
 
 /*
-#cgo CFLAGS: -I./c/custom
+#cgo CFLAGS: -I./c/custom -I./c/include
 #include "c/custom/sys_arch.c"
 */
 import "C"
--- core/tcp_conn.go
+++ core/tcp_conn.go
@@ -41,8 +41,6 @@ import (
 
 type tcpConnState uint
 
-var tcpConns sync.Map
-
 const (
 	// tcpNewConn is the initial state.
 	tcpNewConn tcpConnState = iota
