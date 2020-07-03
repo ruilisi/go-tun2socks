2c84717f56ce120df2fd4a51f272cc445cc22a04
Zhou Yicheng <zyc@paiyou.co>
2020-07-03 15:18:09 +0800

    Update module path

--- proxy/socks/tcp.go
+++ proxy/socks/tcp.go
@@ -7,8 +7,8 @@ import (
 
 	"golang.org/x/net/proxy"
 
-	"github.com/eycorsican/go-tun2socks/common/log"
-	"github.com/eycorsican/go-tun2socks/core"
+	"github.com/ruilisi/go-tun2socks/common/log"
+	"github.com/ruilisi/go-tun2socks/core"
 )
 
 type tcpHandler struct {
