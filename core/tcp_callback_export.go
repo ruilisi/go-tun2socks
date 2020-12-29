7e6340125f8e3ba0acf70352af5876a2a0c89851
Zhou Yicheng <zyc@paiyou.co>
2020-12-29 13:07:47 +0800

    Bring tcp adjusts from trojan-gfw/go-tun2socks

--- core/tcp_callback_export.go
+++ core/tcp_callback_export.go
@@ -1,7 +1,7 @@
 package core
 
 /*
-#cgo CFLAGS: -I./c/include
+#cgo CFLAGS: -I./c/custom -I./c/include
 #include "lwip/tcp.h"
 */
 import "C"
@@ -42,37 +42,43 @@ func tcpAcceptFn(arg unsafe.Pointer, newpcb *C.struct_tcp_pcb, err C.err_t) C.er
 }
 
 //export tcpRecvFn
-func tcpRecvFn(arg unsafe.Pointer, tpcb *C.struct_tcp_pcb, p *C.struct_pbuf, err C.err_t) C.err_t {
-	if err != C.ERR_OK && err != C.ERR_ABRT {
-		return err
-	}
-
+func tcpRecvFn(arg unsafe.Pointer, tpcb *C.struct_tcp_pcb, p *C.struct_pbuf, passedInErr C.err_t) C.err_t {
 	// Only free the pbuf when returning ERR_OK or ERR_ABRT,
 	// otherwise must not free the pbuf.
-	shouldFreePbuf := true
-	defer func() {
-		if p != nil && shouldFreePbuf {
-			C.pbuf_free(p)
+	// lwipMutex.Lock()
+	// defer lwipMutex.Unlock()
+	shouldFreePbuf := false
+	defer func(pb *C.struct_pbuf, shouldFreePbuf *bool) {
+		// lwipMutex.Lock()
+		// defer lwipMutex.Unlock()
+		if pb != nil && *shouldFreePbuf {
+			C.pbuf_free(pb)
+			pb = nil
 		}
-	}()
+	}(p, &shouldFreePbuf)
 
-	conn, ok := tcpConns.Load(getConnKeyVal(arg))
-	if !ok {
-		// The connection does not exists.
+	if passedInErr != C.ERR_OK && passedInErr != C.ERR_ABRT {
+		// TODO: unknown err passed in, not sure if it's correct
 		C.tcp_abort(tpcb)
+		shouldFreePbuf = true
 		return C.ERR_ABRT
 	}
 
+	var conn = (*tcpConn)(arg)
+
 	if p == nil {
 		// Peer closed, EOF.
-		err := conn.(TCPConn).LocalClosed()
+		err := conn.LocalClosed()
 		switch err.(*lwipError).Code {
 		case LWIP_ERR_ABRT:
+			shouldFreePbuf = true
 			return C.ERR_ABRT
 		case LWIP_ERR_OK:
+			shouldFreePbuf = true
 			return C.ERR_OK
 		default:
-			panic("unexpected error")
+			shouldFreePbuf = true
+			return C.ERR_OK
 		}
 	}
 
@@ -86,80 +92,64 @@ func tcpRecvFn(arg unsafe.Pointer, tpcb *C.struct_tcp_pcb, p *C.struct_pbuf, err
 		C.pbuf_copy_partial(p, unsafe.Pointer(&buf[0]), p.tot_len, 0)
 	}
 
-	rerr := conn.(TCPConn).Receive(buf[:totlen])
+	rerr := conn.Receive(buf[:totlen])
 	if rerr != nil {
 		switch rerr.(*lwipError).Code {
 		case LWIP_ERR_ABRT:
+			shouldFreePbuf = true
 			return C.ERR_ABRT
 		case LWIP_ERR_OK:
+			shouldFreePbuf = true
 			return C.ERR_OK
 		case LWIP_ERR_CONN:
-			shouldFreePbuf = false
 			// Tell lwip we can't receive data at the moment,
 			// lwip will store it and try again later.
 			return C.ERR_CONN
 		case LWIP_ERR_CLSD:
-			shouldFreePbuf = false
 			// lwip won't handle ERR_CLSD error for us, manually
 			// shuts down the rx side.
+			shouldFreePbuf = true
+			C.tcp_recved(tpcb, p.tot_len)
 			C.tcp_shutdown(tpcb, 1, 0)
-			return C.ERR_CLSD
+			return C.ERR_OK
 		default:
-			panic("unexpected error")
+			shouldFreePbuf = true
+			return C.ERR_OK
 		}
 	}
 
+	shouldFreePbuf = true
 	return C.ERR_OK
 }
 
 //export tcpSentFn
 func tcpSentFn(arg unsafe.Pointer, tpcb *C.struct_tcp_pcb, len C.u16_t) C.err_t {
-	if conn, ok := tcpConns.Load(getConnKeyVal(arg)); ok {
-		err := conn.(TCPConn).Sent(uint16(len))
-		switch err.(*lwipError).Code {
-		case LWIP_ERR_ABRT:
-			return C.ERR_ABRT
-		case LWIP_ERR_OK:
-			return C.ERR_OK
-		default:
-			panic("unexpected error")
-		}
-	} else {
-		C.tcp_abort(tpcb)
+	var conn = (*tcpConn)(arg)
+
+	err := conn.Sent(uint16(len))
+	switch err.(*lwipError).Code {
+	case LWIP_ERR_ABRT:
 		return C.ERR_ABRT
+	case LWIP_ERR_OK:
+		return C.ERR_OK
+	default:
+		panic("unexpected error conn.Sent()")
 	}
+
 }
 
 //export tcpErrFn
 func tcpErrFn(arg unsafe.Pointer, err C.err_t) {
-	if conn, ok := tcpConns.Load(getConnKeyVal(arg)); ok {
-		switch err {
-		case C.ERR_ABRT:
-			// Aborted through tcp_abort or by a TCP timer
-			conn.(TCPConn).Err(errors.New("connection aborted"))
-		case C.ERR_RST:
-			// The connection was reset by the remote host
-			conn.(TCPConn).Err(errors.New("connection reseted"))
-		default:
-			conn.(TCPConn).Err(errors.New(fmt.Sprintf("lwip error code %v", int(err))))
-		}
-	}
-}
+	var conn = (*tcpConn)(arg)
 
-//export tcpPollFn
-func tcpPollFn(arg unsafe.Pointer, tpcb *C.struct_tcp_pcb) C.err_t {
-	if conn, ok := tcpConns.Load(getConnKeyVal(arg)); ok {
-		err := conn.(TCPConn).Poll()
-		switch err.(*lwipError).Code {
-		case LWIP_ERR_ABRT:
-			return C.ERR_ABRT
-		case LWIP_ERR_OK:
-			return C.ERR_OK
-		default:
-			panic("unexpected error")
-		}
-	} else {
-		C.tcp_abort(tpcb)
-		return C.ERR_ABRT
+	switch err {
+	case C.ERR_ABRT:
+		// Aborted through tcp_abort or by a TCP timer
+		conn.Err(errors.New("connection aborted"))
+	case C.ERR_RST:
+		// The connection was reset by the remote host
+		conn.Err(errors.New("connection reseted"))
+	default:
+		conn.Err(errors.New(fmt.Sprintf("lwip error code %v", int(err))))
 	}
 }
